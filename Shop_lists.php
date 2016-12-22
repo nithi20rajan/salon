f7d75f3a59337c81adc26462fa6ecaa058df53e3
nithi20rajan <nithi20rajan@gmail.com>
2016-12-22 11:33:59 +0530

    Add files via upload

---
+++ Shop_lists.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>Untitled Document</title>
+</head>
+
+<body>
+<table width="628" height="272" border="1">
+  <tr>
+    <td width="274" rowspan="3"><img src="" alt="" name="shopimage" width="268" height="207" id="shopimage" style="background-color: #F0F0F0" /></td>
+    <td width="232" height="43">Name Of Shop:</td>
+    <td width="100" rowspan="3">      <param name="movie" value="button1.swf" />
+      <param name="quality" value="high" />
+      <object classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=5,0,0,0" width="100" height="80" title="Book Button">
+        <param name="movie" value="button2.swf" />
+        <param name="quality" value="high" />
+        <embed src="button2.swf" quality="high" pluginspage="http://www.macromedia.com/shockwave/download/index.cgi?P1_Prod_Version=ShockwaveFlash" type="application/x-shockwave-flash" width="100" height="80" ></embed>
+    </object></td>
+  </tr>
+  <tr>
+    <td height="74">Serivces Offered:<ul><li>Hair Cutting</li>
+        <li> </li>
+    </ul>      </td>
+  </tr>
+  <tr>
+    <td height="130" valign="top"><p>Address: </p></td>
+  </tr>
+</table>
+</body>
+</html>
